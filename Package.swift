c9074a2f99e358bcd219249307b6eea48b97c5a4
Philip <philip.meier@outlook.com>
2024-06-19 11:08:31 -0600

    Fixednaming

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "FeedbackSupplyAppleFramework",
-            targets: ["FeedbackSupplyApplePackageWrapperTarget"]),
+            name: "FeedbackSupply",
+            targets: ["FeedbackSupplyWrapperTarget"]),
     ],
     dependencies: [
         .package(url: "https://github.com/marmelroy/Zip.git", from: "2.1.2")
@@ -21,16 +21,16 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FeedbackSupplyApplePackageWrapperTarget",
+            name: "FeedbackSupplyWrapperTarget",
             dependencies: [
-                .target(name: "FeedbackSupplyAppleFrameworkBinaryTarget"),
+                .target(name: "FeedbackSupplyBinaryTarget"),
                 .product(name: "Zip", package: "zip")
             ],
-            path: "./Sources/FeedbackSupplyApplePackageWrapperTarget"
+            path: "./Sources/FeedbackSupplyWrapperTarget"
         ),
         .binaryTarget(
-            name: "FeedbackSupplyAppleFrameworkBinaryTarget",
-            path: "./Sources/FeedbackSupplyAppleFramework.xcframework"
+            name: "FeedbackSupplyBinaryTarget",
+            path: "./Sources/FeedbackSupply.xcframework"
         )
     ]
 )
